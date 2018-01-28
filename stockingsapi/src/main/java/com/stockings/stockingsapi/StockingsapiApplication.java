284017cdcaef8365ec4ae91508712b58483954e1
Jarek-R-Whiteside <Jarek-R-Whiteside@tsgforce.com>
2018-01-28 14:45:45 -0500

    pushing intial commits for spring boot.

---
+++ stockingsapi/src/main/java/com/stockings/stockingsapi/StockingsapiApplication.java
@@ -0,0 +1,12 @@
+package com.stockings.stockingsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockingsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockingsapiApplication.class, args);
+	}
+}

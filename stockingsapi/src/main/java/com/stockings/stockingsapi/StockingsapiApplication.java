dbddbff67888adaf1c62799dafd7c5b2950c63d8
Yunjia He <heyunjia0503@gmail.com>
2018-01-29 20:54:39 -0500

    Tested Stock API call using restTemplate
    Signed-off-by: Yunjia He <heyunjia0503@gmail.com>

--- stockingsapi/src/main/java/com/stockings/stockingsapi/StockingsapiApplication.java
+++ stockingsapi/src/main/java/com/stockings/stockingsapi/StockingsapiApplication.java
@@ -1,12 +1,24 @@
 package com.stockings.stockingsapi;
 
+import com.stockings.stockingsapi.service.RestfulClientForStockApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan
 public class StockingsapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockingsapiApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
 }
--- stockingsapi/src/main/java/com/stockings/stockingsapi/service/RestfulClientForStockApi.java
+++ stockingsapi/src/main/java/com/stockings/stockingsapi/service/RestfulClientForStockApi.java
@@ -1,8 +1,38 @@
 package com.stockings.stockingsapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.annotation.PostConstruct;
+
 
 @Service
 public class RestfulClientForStockApi {
     //TODO make a restful call to the stock API
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @PostConstruct
+    public String getStockAPI(){
+
+        String apiURL = "https://www.alphavantage.co/query";
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(apiURL)
+                .queryParam("function","TIME_SERIES_INTRADAY")
+                .queryParam("symbol","MSFT")
+                .queryParam("interval","60min")
+                .queryParam("outputsize", "compact")
+                .queryParam("datatype","json")
+                .queryParam("apikey","Z4DGG7FGNDV354HG");
+
+        String response = restTemplate.getForObject(uriComponentsBuilder.toUriString(), String.class);
+
+        System.out.println(response);
+        return response;
+    }
+
 }
